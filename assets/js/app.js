eb478d2f587e0ebc009e0e39ed84b8a314745f7f
Gokul Krishna E <gokul44krishna@gmail.com>
2024-09-08 13:17:34 +0530

    changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,41 +14,27 @@ $(document).ready(function(){
     autoPlay:true,
     infinite: true,
     slidesToShow: 1,
-    prevArrow: "<button class='slick-arrow-button left-arrow'><i class='bx bx-chevron-left'></i></button>",
-    nextArrow: "<button class='slick-arrow-button right-arrow'><i class='bx bx-chevron-right'></i></button>",   
+    prevArrow: "<button class='slick-arrow-button left-arrow'><i class='fas fa-chevron-left'></i></button>",
+    nextArrow: "<button class='slick-arrow-button right-arrow'><i class='fas fa-chevron-right'></i></button>",   
   });
 
   $('.match-height').matchHeight()
 
-  $('input[name="billingAddress"]').change(function(){
-    if($(this).val() === 'sameAddress'){
-      $('.newBillingAddress').hide()
-    }
-    else{
-      $('.newBillingAddress').show()
-    }
+  $('.navbar-toggler').click(function(){
+    $('body').toggleClass('overflow-hidden')
   })
 
-  $('.btn')
-      .on('mouseenter', function(e) {
-        var parentOffset = $(this).offset(),
-            relX = e.pageX - parentOffset.left,
-            relY = e.pageY - parentOffset.top;
-        $(this).find('span').css({top:relY, left:relX})
-      })
-      .on('mouseout', function(e) {
-        var parentOffset = $(this).offset(),
-            relX = e.pageX - parentOffset.left,
-            relY = e.pageY - parentOffset.top;
-        $(this).find('span').css({top:relY, left:relX})
-      });
-
 })
+
 
 $(document).ajaxStop(function(){
   $('.match-height').matchHeight()
   $('.card-content .card-top').matchHeight()
   $('[data-bs-toggle="tooltip"]').tooltip()
+
+  $('.navbar-toggler').click(function(){
+    $('body').toggleClass('overflow-hidden')
+  })
 
   $(window).scroll(function () {
     if ($(this).scrollTop() > 100) {
